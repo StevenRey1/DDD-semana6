5fdc053e60d5dec26c07d45f794bfec3f1cde7de
StevenRey1 <js.reyh1@uniandes.edu.co>
2025-09-14 13:36:11 -0500

    Se realiza clean

--- db-initialization.sql
+++
@@ -1,8 +0,0 @@
-
--- Crear bases de datos dedicadas para cada microservicio que usa PostgreSQL
-CREATE DATABASE pagos_db;
-CREATE DATABASE eventos_db;
-
--- Otorgar todos los privilegios al usuario por defecto de postgres
-GRANT ALL PRIVILEGES ON DATABASE pagos_db TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE eventos_db TO postgres;
